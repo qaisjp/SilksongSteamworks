81d9e2dfa131b9ee7f08b48ab81b242218117618
qaisjp <qaisjp@gmail.com>
2025-09-27 22:28:09 -0400

    add glyphs, add timeline

--- SilksongSteamInput/Plugin.cs
+++ SilksongSteamInput/Plugin.cs
@@ -3,6 +3,7 @@ using BepInEx.Configuration;
 using BepInEx.Logging;
 using GlobalEnums;
 using HarmonyLib;
+using JetBrains.Annotations;
 using Steamworks;
 using System.Runtime.ExceptionServices;
 using UnityEngine;
@@ -30,6 +31,23 @@ namespace SilksongSteamInput
             harmony.PatchAll();
 
             Logger = base.Logger;
+        }
+
+        private void Start()
+        {
+            Logger.LogInfo("Plugin started");
+
+            // Not sure why this is needed when Unity initialises this for us...
+            // maybe the mod has its own "instance" of Steamworks, isolated from
+            // the one that Unity initialises?
+            //
+            // If we don't do this, ActivateActionSet doesn't work.
+            SteamInput.Init(false);
+
+            SetMenu();
+
+            Logger.LogInfo($"Waiting for GameManager to be ready");
+            WaitForGameManager();
         }
 
         // WaitForGameManager runs itself every 2s until GameManager.instance is valid.
@@ -66,23 +84,79 @@ namespace SilksongSteamInput
                     SetInGame();
                 }
             };
+
+            RegularlyFixGlyphs();
         }
 
-        private void Start()
+        // The official way[0] to do this is to call GetDigitalActionOrigins every frame (it's fast)
+        // but we actually need to read the controllers (which may not be as fast) regularly, so
+        // this function calls itself every second.
+        //
+        // [0] See "Step 3.3 - On-screen Glyphs" at https://partner.steamgames.com/doc/features/steam_controller/getting_started_for_devs
+        private void RegularlyFixGlyphs()
         {
-            Logger.LogInfo("Plugin started");
+            Invoke(nameof(RegularlyFixGlyphs), 1.0f);
 
-            // Not sure why this is needed when Unity initialises this for us...
-            // maybe the mod has its own "instance" of Steamworks, isolated from
-            // the one that Unity initialises?
+            var handles = new InputHandle_t[Steamworks.Constants.STEAM_INPUT_MAX_COUNT];
+            int count = SteamInput.GetConnectedControllers(handles);
+
+            InputHandle_t handle;
+            GamepadType? gamepadType = null;
+
+            // Loop over all controllers until we find a controller that needs glyph modifications
+            for (int i = 0; i < count; i++)
+            {
+                handle = handles[i];
+                var inputType = SteamInput.GetInputTypeForHandle(handle);
+                if (inputType == ESteamInputType.k_ESteamInputType_PS5Controller)
+                {
+                    gamepadType = GamepadType.PS5;
+                    break;
+                }
+                else if (inputType == ESteamInputType.k_ESteamInputType_PS4Controller)
+                {
+                    gamepadType = GamepadType.PS4;
+                    break;
+                }
+                else if (inputType == ESteamInputType.k_ESteamInputType_PS3Controller)
+                {
+                    gamepadType = GamepadType.PS3_WIN;
+                    break;
+                }
+                else
+                {
+                    Logger.LogInfo($"RegularlyFixGlyphs: Skipping unknown controller type {inputType}");
+                }
+            }
+
+            if (gamepadType == null)
+            {
+                // No known gamepad found, nothing to do
+                return;
+            }
+
+            // Two options:
+            // 1. either update the activeGamepadType, which is far reaching and could cause problems with input
+            // 2. or copypaste and patch UIButtonSkins to use our detected gamepad type instead of activeGamepadType
             //
-            // If we don't do this, ActivateActionSet doesn't work.
-            SteamInput.Init(false);
+            // based on user reports we might need to switch from (1) to (2)
 
-            SetMenu();
+            var currActiveGamepadType = InputHandler.Instance.activeGamepadType;
+            if (currActiveGamepadType != gamepadType)
+            {
+                Logger.LogInfo($"RegularlyFixGlyphs: Setting activeGamepadType from {currActiveGamepadType} to {gamepadType}");
+                InputHandler.Instance.activeGamepadType = gamepadType.Value;
+            }
+        }
 
-            Logger.LogInfo($"Waiting for GameManager to be ready");
-            WaitForGameManager();
+        private void Update()
+        {
+            if (Input.GetKeyDown("k"))
+            {
+                Logger.LogInfo("K pressed, killing self");
+                DamageTag.DamageTagInstance dmg = new DamageTag.DamageTagInstance { amount = 100 };
+                HeroController.instance.ApplyTagDamage(damageTagInstance: dmg);
+            }
         }
 
         private void SetMenu()
@@ -97,6 +171,24 @@ namespace SilksongSteamInput
             var actionSet = SteamInput.GetActionSetHandle("InGameControls");
             SteamInput.ActivateActionSet(allHandles, actionSet);
             Logger.LogInfo($"Set to InGameControls, actionSet: {actionSet}");
+        }
+
+        // PostFix HeroController.Start to add a OnDeath handler
+        [HarmonyPatch(typeof(HeroController), nameof(HeroController.Start))]
+        class HeroController_Start_Patch
+        {
+            [HarmonyPostfix]
+            static void Postfix(HeroController __instance)
+            {
+                Logger.LogInfo($"HeroController.Start postfix, hooking OnDeath {__instance}");
+                __instance.OnDeath += OnDeath;
+            }
+        }
+
+        private static void OnDeath()
+        {
+            Logger.LogInfo($"OnDeath fired");
+            SteamTimeline.AddTimelineEvent("steam_death", "Killed", null, 0, 0, 0, ETimelineEventClipPriority.k_ETimelineEventClipPriority_Standard);
         }
     }
 }
